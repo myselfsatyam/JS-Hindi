1aae5c92759649f9acfe169d4ad66895e15b4f9d
myselfsatyam <137916348+myselfsatyam@users.noreply.github.com>
2024-05-17 17:09:09 +0000

    conatiner config

--- 03-basics/01_functions.js
+++ 03-basics/01_functions.js
@@ -40,4 +40,31 @@ if(!username){
 
 
 //console.log(LoginUsermessage("Satyam"));
-console.log(LoginUsermessage()); //when we dont pass arguements it gives undefined as output
+//console.log(LoginUsermessage()); //when we dont pass arguements it gives undefined as output
+
+function CalculatecartPrize(val1,val2,...num1){
+    return num1
+
+}
+ //console.log(CalculatecartPrize(200,400,600,800))
+
+ const user ={
+    username:"Satyam",
+    price:200
+ }
+
+ function handleObject(anyobject){
+    console.log(` Username is ${anyobject.username} and price is ${anyobject.price}`);
+ }
+
+ //handleObject(user)
+ 
+ const NewArray=[200,400,600,800]
+
+ function returnSecondvalue(getArray){
+    return getArray[1]
+
+ }
+
+ //console.log(returnSecondvalue(NewArray));
+ console.log(returnSecondvalue([200,400,600,800]));
