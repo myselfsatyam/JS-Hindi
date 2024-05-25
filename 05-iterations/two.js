7b950cd84a17e71bbd6a4f747c61353ab0e8f4c1
myselfsatyam <137916348+myselfsatyam@users.noreply.github.com>
2024-05-25 18:12:08 +0000

    loop discussions

---
+++ 05-iterations/two.js
@@ -0,0 +1,19 @@
+let index =0
+// while(index<=10){
+//     console.log(`Value of index is :${index}`);
+//     index=index +2
+// }
+
+let MyArray=['Iron','Spidey','Cap']
+
+let arr=0
+while(arr< MyArray.length){
+    //console.log(`Value is ${MyArray[arr]}`);
+    arr++
+}
+
+let score=11
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10)
